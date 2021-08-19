2aca1655f4b61d239fc6b91d10d932179df78c07
marifrahmawan <m1600018181@webmail.uad.ac.id>
2021-08-19 23:19:53 +0800

    adding bump animation on card

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 
@@ -6,12 +6,33 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const [buttonBump, setButtonBump] = useState(false);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonBump(true);
+
+    const bumpEffect = setTimeout(() => {
+      setButtonBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(bumpEffect);
+    };
+  }, [items]);
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${buttonBump ? classes.bump : ''}`;
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
